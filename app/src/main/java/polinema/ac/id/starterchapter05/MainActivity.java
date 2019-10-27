a607b32b29fba19db1f4b7b4b44af6d6242076a9
Muchammad Arsyad <arsyad1822@gmail.com>
2019-10-27 17:57:45 +0700

    update

--- app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
@@ -2,7 +2,11 @@ package polinema.ac.id.starterchapter05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import polinema.ac.id.starterchapter05.activities.StaticActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void handlerPraktikumFragment(View view) {
+        Intent intent = new Intent(this, StaticActivity.class);
+        startActivity(intent);
+    }
+
+    public void handlerDynamicFragment(View view) {
+        Intent intent = new Intent(this, StaticActivity.class);
+        startActivity(intent);
+    }
+
+    public void handlerStaticFragment(View view) {
+        Intent intent = new Intent(this, StaticActivity.class);
+        startActivity(intent);
+    }
+
+
+
 }
---
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/DynamicActivity.java
@@ -0,0 +1,25 @@
+package polinema.ac.id.starterchapter05.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import polinema.ac.id.starterchapter05.R;
+
+public class DynamicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dynamic);
+    }
+
+    public void handleClickLoadRedFragment (View view) {
+
+    }
+
+    public void handleClickLoadBlueFragment (View view) {
+
+    }
+}
